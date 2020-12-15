516fa1dc7f9c74a3ebbbb6ecff187dcbb3228371
maurischults <mauri.schults@khk.ee>
2020-12-15 18:58:05 +0200

    first commit

---
+++ weather.js
@@ -0,0 +1,16 @@
+class Weather {
+  constructor(city) {
+    this.city = city;
+    this.key = "d316501f973d45f015e382552e3af831";
+  }
+  async weatherData() {
+    const resp = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        this.city +
+        "&appid=" +
+        this.key
+    );
+    const data = await resp.json();
+    return data;
+  }
+}
