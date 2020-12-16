3028cc81d3ed93e4318d860ea65e0c80c37c8675
maurischults <mauri.schults@khk.ee>
2020-12-16 20:20:04 +0200

    lisatud niiskuse nait

--- UI.js
+++ UI.js
@@ -3,11 +3,13 @@ class UI {
     this.cityName = document.querySelector("#w-city");
     this.temp = document.querySelector("#w-temp");
     this.desc = document.querySelector("#w-desc");
+    this.humi = document.querySelector("#w-humi");
   }
   print(cityWeather) {
     this.cityName.textContent = cityWeather.name;
     const celsius = Math.round(parseFloat(cityWeather.main.temp) - 275.15);
     this.temp.innerHTML = `${celsius} &deg;C`;
-    this.desc.textContent = cityWeather.weather(0).description;
+    this.desc.textContent = cityWeather.weather[0].description;
+    this.humi.textContent = cityWeather.main.humidity + `%`;
   }
 }
