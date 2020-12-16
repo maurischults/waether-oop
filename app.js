b6ffde4f620ae663e7cc522f12f0f6549527eb16
maurischults <mauri.schults@khk.ee>
2020-12-16 20:44:11 +0200

    Valmis

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const weather = new Weather("Tartu linn");
 const ui = new UI();
-weather
-  .weatherData()
-  .then((cityWeather) => {
-    ui.print(cityWeather);
-  })
-  .catch((error) => console.log(error));
-console.log(ui);
+//default city waether
+document.addEventListener("DOMContentLoaded", drawWeather);
+
+// change city
+const changeBtn = document.querySelector("#w-change");
+changeBtn.addEventListener("click", changeWeather);
+
+function changeWeather() {
+  const city = document.querySelector("#city").value;
+  weather.changeCityName(city);
+  drawWeather();
+  $("#changeCity").modal("hide");
+}
+
+function drawWeather() {
+  weather
+    .weatherData()
+    .then((cityWeather) => {
+      ui.print(cityWeather);
+    })
+    .catch((error) => console.log(error));
+}
